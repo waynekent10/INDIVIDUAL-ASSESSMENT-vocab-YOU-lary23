169727c70856f44fa99becf5cae9a56a6673398b
Bruce Clark <Bruce.clark55@gmail.com>
2023-12-03 23:35:02 -0500

    added dombuilder to startApp

--- utils/startApp.js
+++ utils/startApp.js
@@ -1,8 +1,10 @@
 import logoutButton from '../components/logoutButton';
+import domBuilder from '../components/shared/domBuilder';
 import navBar from '../components/shared/navBar';
 import navigationEvents from '../events/navigationEvents';
 
 const startApp = () => {
+  domBuilder();
   navBar();
   logoutButton();
   navigationEvents();
