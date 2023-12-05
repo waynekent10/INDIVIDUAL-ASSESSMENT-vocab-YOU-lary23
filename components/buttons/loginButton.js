5afe9a186ce66d3b2c882e93d43e158fc7a7515e
Bruce Clark <Bruce.clark55@gmail.com>
2023-12-04 21:24:05 -0500

    made changes to the login button

--- components/buttons/loginButton.js
+++ components/buttons/loginButton.js
@@ -3,7 +3,7 @@ import { signIn } from '../../utils/auth';
 // GOOGLE LOGIN BUTTON
 const loginButton = () => {
   const domString = '<h1>Welcome to Vocab-YOU-lary</h1><button id="google-auth" class="btn btn-success">LOGIN</button>';
-  document.querySelector('#login-form-container').innerHTML = domString;
+  document.querySelector('#app').innerHTML = domString;
   document.querySelector('#google-auth').addEventListener('click', signIn);
 };
 
