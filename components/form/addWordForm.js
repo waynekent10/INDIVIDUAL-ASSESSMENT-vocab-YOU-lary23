302652aa7bb3bceb44e26f7d605806fefe91a1d8
Bruce Clark <Bruce.clark55@gmail.com>
2023-12-04 19:33:00 -0500

    corrected errors within the add word form

--- components/form/addWordForm.js
+++ components/form/addWordForm.js
@@ -4,15 +4,22 @@ import renderToDOM from '../../utils/renderToDom';
 const addWordForm = (obj = {}) => {
   clearDom();
   const domString = `
-    <form id="${obj.firebaseKey ? `update-word--${obj.firebaseKey}` : 'create-entry'}" class="mb-4">
+    <form id="${obj.firebaseKey ? `update-word--${obj.firebaseKey}` : 'submit-entry'}" class="mb-4">
     <div class="form-group">
-      <label for="title">Book Title</label>
-      <input type="text" class="form-control" id="title" aria-describedby="bookTitle" placeholder="Enter a word or phrase" value="${obj.word || ''}" required>
+      <label for="word">Word</label>
+      <input type="text" class="form-control" id="wordVocab" aria-describedby="bookTitle" placeholder="Enter a word or phrase" value="${obj.word || ''}" required>
     </div>
     <div class="form-group">
-    <label for="description">Description</label>
+    <label for="definition">Definition</label>
     <textarea class="form-control" placeholder="Enter a definition" id="definition" style="height: 100px">${obj.definition || ''}</textarea>
   </div>
   <div class="form-group">
+  <div class="form-group" id="select-language">
+  </div> <button type="submit" class="btn btn-primary">Submit Word
+  </button>
+</form>`;
 
+  renderToDOM('#form-container', domString);
 };
+
+export default addWordForm;
