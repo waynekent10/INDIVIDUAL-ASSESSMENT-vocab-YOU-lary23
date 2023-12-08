26c074c24812637c366f8e6b088a4b80153f2180
Bruce Clark <Bruce.clark55@gmail.com>
2023-12-07 21:15:35 -0500

    user factoring

--- components/shared/navBar.js
+++ components/shared/navBar.js
@@ -5,7 +5,6 @@ const navBar = () => {
   <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark mb-5">
   <div class="container-fluid">
     <a class="navbar-brand" href="#" id="home">
-      <img src="..." alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
       Vocab-You-lary
     </a>
     <div class="collapse navbar-collapse" id="navbarNav">
--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -3,18 +3,18 @@ import { showWords } from '../pages/vocab';
 import addWordForm from '../components/form/addWordForm';
 import { signOut } from '../utils/auth';
 
-const navigationEvents = () => {
+const navigationEvents = (user) => {
 // LOGOUT BUTTON
   document.querySelector('#logout-button')
     .addEventListener('click', signOut);
 
   // HOME PAGE
   document.querySelector('#home').addEventListener('click', () => {
-    getWords().then(showWords);
+    getWords(user).then(showWords);
   });
   // CREATE WORD
   document.querySelector('#createEntry').addEventListener('click', () => {
-    addWordForm({});
+    addWordForm({}, user);
   });
 };
 
